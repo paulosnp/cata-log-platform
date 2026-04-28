e618868456f82c6f16e44566ac5acbb4bda286cf
Paulo Cardoso <paulocardoso64h@gmail.com>
2026-04-27 23:21:03 -0300

    att de teste

--- catalog-web/src/App.jsx
+++ catalog-web/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         </p>
       </div>
 
-      <p className="footer-info">Cata Log Platform &bull; v0.1.0</p>
+      <p className="footer-info">Cata Log Platform &bull; v0.1.1</p>
     </div>
   )
 }
